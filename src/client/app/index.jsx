2bb652ebef9974dc5352659d444fa2c9a0aae145
timlkelly <timlkelly@gmail.com>
2016-07-19 17:46:01 -0500

    add moment and perpage

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 var ReactBsTable  = require('react-bootstrap-table');
 var BootstrapTable = ReactBsTable.BootstrapTable;
 var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
+var moment = require('moment');
 
 var CitadelTable = React.createClass({
   render() {
     return (
-      <BootstrapTable data={this.props.citadels} striped={true} hover={true} pagination={true} search={true}>
+      <BootstrapTable data={this.props.citadels} 
+                      hover={true}
+                      search={true}
+                      striped={true}
+                      pagination={true}
+                      options={ {
+                        sizePerPage: 10,
+                        sizePerPageList: [5, 10, 20]
+                      } }>
         <TableHeaderColumn isKey={true} dataField="id" hidden={true}>Citadel Id</TableHeaderColumn>
         <TableHeaderColumn dataField="system">System</TableHeaderColumn>
         <TableHeaderColumn dataField="region">Region</TableHeaderColumn>
@@ -25,9 +34,15 @@ var App = React.createClass({
     return { citadelData: [] };
   },
   componentDidMount: function() {
-    fetch(process.env.CITADEL_URL).then(function(response) {
+    fetch(process.env.CITADEL_URL)
+    .then(function(response) {
       return response.json();
     }).then(function(response_data) {
+      response_data.citadels.map(function(c) {
+        if (c.killed_at != null) {
+          c.killed_at = moment(c.killed_at, 'X').toString()
+        }
+      })
       this.setState( {citadelData: response_data.citadels } )
     }.bind(this))
   },
